1c087ccba5c3287e9503a843a4a24c84ba01d53b
FaithHQ <olusesiolutobi@gmail.com>
2022-11-07 16:35:06 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,36 +1,32 @@
-import React, {component} from "react";
-import ReactDOM from "react-dom";
+//****************************** Alt-School Africa Exam******************************************************************
+//                       NAME: OLUTOBI OLUSESI
+//                               ID: ALT/SOE/022/5749
+//                       SCHOOL: ENGINEERING
+//                               TRACK: FRONT-END ENGINEERING
+/** PROJECT 2
+Setup a custom counter hook with increment, decrement, reset, setValue functions
+with a valid UI and implement a combination of states with a useReducer that 
+implements a counter with the four evident features in the custom hooks - 
+increment, decrement, reset, setValue. Implement a page for the custom hook, useReducer, 404, 
+and a page to test error boundary and good seo
+ */
+//****************************************************************************************************************
+import React, {Component} from "react";
+import ReactDOM from "react-dom/client";
 
-class ErrorBoundaryBranch extends component {
+
+class ErTestTemplate extends Component {
   constructor(props) {
-    this.super(props);
+    super(props);
     this.state = {hasError: false};
   }
   componentDidCatch(error, info) {
     return this.setState({hasError: true})
   }
   render() {
-    if(this.state.hasError === true) {
-      return <h1>{/* Type here, inside this h1 tag
-          "The Name of the Component" and write a custom error of any type you want */}Error Here</h1>
-  }
-
-    return (
-      <div>
-          {/*this.props.children*/}
-      </div>
-    )
+    if(this.state.hasError) {
+      return <h1>{/**Name the error here */} </h1>
+    }
+    return this.props.children
   }
 }
-
-
-export default ErrorBoundaryBranch
-
-
-
-
-
-
-
-
-//Copy and paste in new files(multiple)  to reuse this ErrorBoundaryBranch component
